238cfbb9aa0ddcc6f1947f199966940b7f13412c
Andrea Titton <andrea.titton.ext@accurat.it>
2019-01-16 12:58:52 +0100

    🛠 new build

---
+++ src/components/GeneticPicture.js
@@ -0,0 +1,9 @@
+// Code from http://www.petercollingridge.co.uk/blog/evolving-images/
+
+import React from 'react'
+
+export class GeneticPicture extends React.Component {
+  render() {
+    return <></>
+  }
+}

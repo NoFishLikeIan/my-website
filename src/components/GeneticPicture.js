ac3c43c4a85bf93c8e0bf414857c455547b2dc75
Andrea Titton <andrea.titton.ext@accurat.it>
2019-01-18 10:41:35 +0100

    ⚠️   moved logic to GPU js we are now getting a WEBGL error 😱

--- src/components/GeneticPicture.js
+++ src/components/GeneticPicture.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import getPixels from 'get-pixels'
-import { range, clamp, flatten, sum } from 'lodash'
+import { range, clamp, flatten, sum, zip } from 'lodash'
 import GPU from 'gpu.js'
 import { Circle, Stage, Layer } from 'react-konva'
 
@@ -10,26 +10,70 @@ const STEP = 255 * 0.1
 const IMAGE = '/100x100.jpg'
 const MIN_RADIUS = 1
 const MAX_RADIUS = 5
+
 const random = () => Math.random() * 2 - 1
-
-const modifyChannel = c => Math.floor(clamp(c + random() * STEP, 0, 255))
-
-const modifyCoord = (c, radius, max) => Math.floor(clamp(c + random() * radius, 0, max))
-
-const modifyRadius = currentRadius =>
-  Math.floor(clamp(currentRadius + random(), MIN_RADIUS, MAX_RADIUS))
 
 const randomChannel = () => Math.floor(Math.random() * 255)
 
-const squaredError = (trueArray, predictedArray) => {
-  return trueArray.reduce(
-    (acc, _, index, array) => acc + Math.pow(array[index] - predictedArray[index], 2),
-  )
+function modifyInstance(instanceArray, nInstanceTimesFeautureRandomNumbers, maxX, maxY, allPixels) {
+  var instance = instanceArray[this.thread.x]
+  var _random = nInstanceTimesFeautureRandomNumbers[this.thread.x]
+  var radius = instanceArray[instanceArray.length - 1]
+
+  var r = instance[0]
+  var g = instance[1]
+  var b = instance[2]
+  var a = instance[3]
+  var x = instance[4]
+  var y = instance[5]
+
+  var newR = Math.max(Math.min(r + _random[0] * 255, 255), 0)
+  var newG = Math.max(Math.min(g + _random[1] * 255, 255), 0)
+  var newB = Math.max(Math.min(b + _random[2] * 255, 255), 0)
+  var newA = Math.max(Math.min(a + _random[3] * 255, 255), 0)
+
+  var newX = Math.max(Math.min(x + _random[4] * radius, maxX), 0)
+  var newY = Math.max(Math.min(y + _random[5] * radius, maxY), 0)
+
+  var newRadius = Math.max(Math.min(radius + _random[6] * radius, 5), 1)
+
+  // Find points under the new circle
+  var relevantPixels = []
+  var extentX = [Math.floor(newX - newRadius), Math.floor(newX + radius)]
+  var extentY = [Math.floor(newY - newRadius), Math.floor(newY + radius)]
+
+  var deltaX = Math.abs(extentX[1] - extentX[0])
+  var deltaY = Math.abs(extentY[1] - extentY[0])
+
+  for (var w = 0; w < deltaX; w++) {
+    for (var z = 0; z < deltaY; z++) {
+      var rgbaOfThisPixel = allPixels[(w, z)]
+      relevantPixels.push(rgbaOfThisPixel)
+    }
+  }
+
+  var n = relevantPixels.length
+  var sumSquared = 0
+
+  for (var i = 0; i < n; i++) {
+    var rgbaTarget = relevantPixels[i]
+    var squaredErrorR = (rgbaTarget[0] - newR) * (rgbaTarget[0] - newR)
+    var squaredErrorG = (rgbaTarget[1] - newG) * (rgbaTarget[1] - newG)
+    var squaredErrorB = (rgbaTarget[2] - newB) * (rgbaTarget[2] - newB)
+    var squaredErrorA = (rgbaTarget[3] - newA) * (rgbaTarget[3] - newA)
+    var thisPixelSumSquare = squaredErrorR + squaredErrorG + squaredErrorB + squaredErrorA
+
+    var squareRoot = Math.sqrt(thisPixelSumSquare)
+
+    sumSquared = squareRoot + sumSquared
+  }
+
+  var fitness = sumSquared / n
+  return [fitness, r, g, b, a, x, y, radius]
 }
 
 class Instance {
-  constructor(maxX, maxY, getPixelFn, id) {
-    this.getPixel = getPixelFn
+  constructor(maxX, maxY, id) {
     this.maxX = maxX
     this.maxY = maxY
     this.y = Math.random() * maxY
@@ -48,8 +92,7 @@ class Instance {
       Math.floor(clamp(this.y + this.radius, 0, maxY)),
     ]
 
-    this.currentFitness = this.fitness(this.xExtent, this.yExtent, this.rgbaValue, this.radius)
-    this.id = id
+    this.currentFitness = 0
   }
 
   get rgbaValue() {
@@ -57,22 +100,12 @@ class Instance {
     return [r, g, b, a]
   }
 
-  fitness(xExtent, yExtent, rgbaValue, radius) {
-    // Circle
-    const allPixels = flatten(
-      range(...xExtent).map(x =>
-        range(...yExtent).map(y => this.getPixel(Math.floor(x), Math.floor(y))),
-      ),
-    )
-
-    const pixelLength = allPixels.length || 1
-    const squareErrors =
-      sum(allPixels.map(pixel => squaredError(pixel, rgbaValue))) / pixelLength + radius * 0.01
-
-    return 1 / Math.sqrt(squareErrors)
+  get instanceData() {
+    const { r, g, b, a, x, y, radius } = this
+    return [r, g, b, a, x, y, radius]
   }
 
-  set newChildren({ r, g, b, a, x, y, radius, xExtent, yExtent }) {
+  set newChildren({ r, g, b, a, x, y, radius }) {
     this.r = r
     this.g = g
     this.b = b
@@ -80,47 +113,27 @@ class Instance {
     this.x = x
     this.y = y
     this.radius = radius
-    this.xExtent = xExtent
-    this.yExtent = yExtent
   }
 
-  modify() {
-    const r = modifyChannel(this.r)
-    const g = modifyChannel(this.g)
-    const b = modifyChannel(this.b)
-    const a = modifyChannel(this.a)
-    const x = modifyCoord(this.x, this.radius, this.maxX)
-    const y = modifyCoord(this.y, this.radius, this.maxY)
-    const radius = modifyRadius(this.radius)
-
-    const xExtent = [
-      Math.floor(clamp(x - radius, 0, this.maxX)),
-      Math.floor(clamp(x + radius, 0, this.maxX)),
-    ]
-    const yExtent = [
-      Math.floor(clamp(y - radius, 0, this.maxY)),
-      Math.floor(clamp(y + radius, 0, this.maxY)),
-    ]
-
-    const rgb = [r, g, b, a]
-
-    const childFitness = this.fitness(xExtent, yExtent, rgb, radius)
-
+  modify(childFitness, r, g, b, a, x, y, radius) {
     if (childFitness > this.currentFitness) {
-      this.newChildren = { r, g, b, a, x, y, radius, xExtent, yExtent }
+      this.newChildren = { r, g, b, a, x, y, radius }
       this.currentFitness = childFitness
     }
   }
 }
 
 class GeneticAlgorithm {
-  constructor(width, height, getPixelFn, n = 10000) {
+  constructor(width, height, ndPixelArray, n = 10000) {
     this.width = width
     this.height = height
-    this.getPixelFn = getPixelFn
-    this.offSpring = range(n).map(idx => new Instance(width, height, getPixelFn, idx))
+    this.ndPixelArray = ndPixelArray
+
+    this.actuallyEveryPixel = getAllPixels(ndPixelArray)
+    this.offSpring = range(n).map(idx => new Instance(width, height, idx))
 
     this.gpu = new GPU()
+    this.mutate = this.gpu.createKernel(modifyInstance, { output: [this.offSpring.length] })
   }
 
   get allData() {
@@ -131,7 +144,21 @@ class GeneticAlgorithm {
   }
 
   newGeneration() {
-    this.offSpring.forEach(_ => _.modify())
+    const instanceArray = this.offSpring.map(instance => instance.instanceData)
+    const nInstanceTimesFeautureRandomNumbers = range(this.offSpring.length).map(random)
+    const maxX = this.width
+    const maxY = this.height
+    const allPixels = this.actuallyEveryPixel
+
+    const allNewData = this.mutate(
+      instanceArray,
+      nInstanceTimesFeautureRandomNumbers,
+      maxX,
+      maxY,
+      allPixels,
+    )
+
+    this.offSpring.forEach((instance, index) => instance.modify(...allNewData[index]))
   }
 }
 
@@ -141,6 +168,21 @@ function getPixel(pixels) {
   return (i, j) => {
     return range(4).map(n => pixels.get(i, j, n))
   }
+}
+
+function getAllPixels(pixels) {
+  const [width, height, n] = pixels.shape
+
+  const allPixelData = range(width).map(x =>
+    range(height).map(y =>
+      range(n).map(channel => {
+        const pixelDatum = pixels.get(x, y, channel)
+        return pixelDatum
+      }),
+    ),
+  )
+
+  return allPixelData
 }
 
 export class GeneticPicture extends React.Component {
@@ -160,16 +202,10 @@ export class GeneticPicture extends React.Component {
       return
     }
     console.log('Fetched pixels')
-    const pixelGetter = getPixel(pixels)
-    const genAlgo = new GeneticAlgorithm(pixels.shape[0], pixels.shape[1], pixelGetter)
+    const genAlgo = new GeneticAlgorithm(pixels.shape[0], pixels.shape[1], pixels)
 
     this.genId = setInterval(this.updateGeneration, 16)
     this.setState({ pixels, genAlgo })
-  }
-
-  generateAlgorithm(width, height, pixelGetter) {
-    const genAlgo = new GeneticAlgorithm(width, height, pixelGetter)
-    return genAlgo
   }
 
   componentDidMount() {
